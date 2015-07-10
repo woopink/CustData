6809b414edc3f7b5302ace8be0f74039e1b2b512
woopink <howardlovegerman@gmail.com>
2015-07-11 07:17:52 +0800

    Item_Two

---
+++ Cust/Controllers/DefaultController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Cust.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult RdlcReportDemo()
+        {
+            string aspx = "/Reports/CustView.aspx";
+            using (var sw = new StringWriter())
+            {
+                System.Web.HttpContext.Current.Server.Execute(aspx, sw, true);
+                return Content(sw.ToString());
+            }
+        }
+    }
+}
---
+++ Cust/Reports/CustView.aspx.cs
@@ -0,0 +1,31 @@
+﻿using Cust.Models;
+using Microsoft.Reporting.WebForms;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Cust.Reports
+{
+    public partial class CustView : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            客戶資料Entities db = new 客戶資料Entities();
+              
+            this.ReportViewer1.Reset();
+            this.ReportViewer1.LocalReport.Dispose();
+            this.ReportViewer1.LocalReport.DataSources.Clear();
+
+            var  dt = db.CustView.ToList();
+            ReportDataSource datasource = new ReportDataSource("Dataset1",dt);
+
+            this.ReportViewer1.LocalReport.ReportPath = Server.MapPath("~/Reports/Cust.rdlc");
+            this.ReportViewer1.LocalReport.DataSources.Add(datasource);
+            this.ReportViewer1.LocalReport.Refresh();
+        }
+    }
+}
