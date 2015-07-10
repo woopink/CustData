5525eb7cf35b56fcec4ef87558cea50e7dd81a59
howardlovegerman <howardlovegerman@gmail.com>
2015-07-10 13:34:53 +0800

    Item_One

---
+++ Cust/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Cust.Startup))]
+namespace Cust
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
